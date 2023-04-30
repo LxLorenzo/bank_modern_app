87ed0aa95796935cd95801c2dddd477484c4d88f
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-04-30 02:55:48 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
+	const [toggle, setToggle] = useState(false);
 
-    </nav>
-  )
-}
+	return (
+		<nav className="w-full flex py-6 justify-between items-center navbar">
+			<img src={logo} alt="Logo" className="w-[124px] h[32px]" />
 
-export default Navbar
+			<ul className="list-none sm:flex hidden justify-end items-center flex-1">
+				{navLinks.map((nav, index) => (
+					<li
+						key={nav.id}
+						className={`font-poppins font-normal cursor-pointer text-[16px] ${
+							index === navLinks.length - 1 ? "mr-0" : "mr-10"
+						} text-white`}
+					>
+						<a href={`#${nav.id}`}>{nav.title}</a>
+					</li>
+				))}
+			</ul>
+
+			<div className="sm:hidden flex flex-1 justify-end items-center">
+				<img
+					src={toggle ? close : menu}
+					alt="Menu"
+					className="w-[28px] h-[28px] object-content"
+					onClick={() => setToggle((prev) => !prev)}
+				/>
+
+        <div className={`${toggle} ? 'flex' : 'hidden'`}>
+
+        </div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
